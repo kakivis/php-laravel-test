f881a718cc41ff5a2d5328294a439d29bc55ec19
Everton Barreto <junior_kakinhu@hotmail.com>
2020-06-03 15:28:30 -0300

    Add search bar at composite report

--- resources/views/composite_report/index.blade.php
+++ resources/views/composite_report/index.blade.php
@@ -6,23 +6,48 @@
             <div class="title m-b-md">
                 Composite Report
             </div>
-            <table>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Number of logins</th>
-                    <th>Active</th>
-
-                </tr>
-                @foreach($users as $user)
+            <div>
+                <form class="search-bar" action="/composite_report" method="POST">
+                    @csrf
+                    <label for="name">Name:</label>
+                    <input type="text" id="name" name="name">
+                    <label for="email">Email:</label>
+                    <input type="text" id="email" name="email">
+                    <label for="status">Status:</label>
+                    <select id="status" name="status">
+                        <option value="1">Active</option>
+                        <option value="0">Inactive</option>
+                    </select>
+                    <br>
+                    <label for="start-date">Start date:</label>
+                    <input type="datetime-local" id="start-date" name="start_date">
+                    <label for="end-date">End date:</label>
+                    <input type="datetime-local" id="end-date" name="end_date">
+                    <input type="submit" value="Search">
+                    <br>
+                    <input class="align-left" type="button" value="Top 10 least logged in" onclick="window.location.href='/composite_report/least_logged'" />
+                    <input class="align-right" type="button" value="Top 10 most logged in" onclick="window.location.href='/composite_report/most_logged'" />
+                </form>
+            </div>
+            <div class="table-container">
+                <table>
                     <tr>
-                        <td>{{$user->name}}</td>
-                        <td>{{$user->email}}</td>
-                        <td>{{count($user->user_accesses)}}</td>
-                        <td>{{$user->active ? 'Active' : 'Inactive'}}</td>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Number of logins</th>
+                        <th>Active</th>
+
                     </tr>
-                @endforeach
-            </table>
+                    @foreach($users as $user)
+                        <tr>
+                            <td>{{$user->name}}</td>
+                            <td>{{$user->email}}</td>
+                            <td>{{count($user->user_accesses)}}</td>
+                            <td>{{$user->active ? 'Active' : 'Inactive'}}</td>
+                        </tr>
+                    @endforeach
+                </table>
+            </div>
 
             <div class="links">
                 <a href="/"><- Back to index</a>
