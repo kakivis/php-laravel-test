cbd39b2e6430c629b7cfe56f00861cbc72762778
Everton Barreto <junior_kakinhu@hotmail.com>
2020-06-03 15:52:58 -0300

    Add most/least logged handling

--- app/Http/Controllers/CompositeReportController.php
+++ app/Http/Controllers/CompositeReportController.php
@@ -10,4 +10,14 @@ class CompositeReportController extends Controller {
         $users = User::all();
         return view('composite_report.index', ['users' => $users]);
     }
+
+    public function least_logged() {
+        $users = User::withCount('user_accesses')->orderBy('user_accesses_count', 'asc')->paginate(10);
+        return view('composite_report.index', ['users' => $users]);
+    }
+
+    public function most_logged() {
+        $users = User::withCount('user_accesses')->orderBy('user_accesses_count', 'desc')->paginate(10);
+        return view('composite_report.index', ['users' => $users]);
+    }
 }
